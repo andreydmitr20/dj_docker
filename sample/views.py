4dc20df8cba3c04a98af41e2da83ba702a7096c6
andreydmitr21 <andreydmitr20@gmail.com>
2023-02-09 20:13:58 +0100

    test json

--- sample/views.py
+++ sample/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from dj_docker_drf.postgres_db import Postgres_db
 
 from rest_framework.views import APIView
 from django.http import JsonResponse
@@ -8,5 +9,20 @@ class HomeView(APIView):
 
     def get(self, request, format=None):
         print(request)
+        # return JsonResponse({"message":
+        #  'HELLO D112 FROM DJANGO AND DOCKER'})
+        d = Postgres_db('postgres',
+                        'postgres',
+                        'changeme')
+        d.connect()
+        json = {}
+        if (d.is_connected()):
+            json = d.get_json(
+                '''SELECT * from "user"''',
+                'user_id',
+                ['name', 'api_key'])
+            d.disconnect()
+            print(json)
+            return JsonResponse(json)
         return JsonResponse({"message":
-                             'HELLO D112 FROM DJANGO AND DOCKER'})
+                             'Error getting ddata from server'})
